99630b3aa04191921856c429fe6fa32fd53a89ea
HiLaVine <sinnafa01@gmail.com>
2024-11-07 23:02:37 -0600

    modulo 1 completado, modulo 2 2.0.8

---
+++ modulo2/variables.js
@@ -0,0 +1,35 @@
+"use strict";
+/*var height; 
+console.log(height); // -> undefined
+console.log(weight); // -> Uncaught ReferenceError: weight is not defined*/
+
+/*let height=174;
+let anotherheight=height;
+
+console.log(height);
+console.log(anotherheight+1);
+
+let weight;
+weight = 90;
+
+console.log(weight);*/
+
+/*
+let hola = "hello";
+console.log(hola);*/
+
+/*let  steps  =  100;
+console.log(steps);  //  ->  100
+steps  =  120;  //  ->  120
+console.log(steps);
+steps  =  steps  +  200;
+console.log(steps);  //  ->  320*/
+
+/*let greeting = "hello";
+let counter = 100;
+
+console.log(greeting + counter);*/
+
+const greeting = "hello";
+greeting = "hi";
+
