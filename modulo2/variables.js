ff4c8a3df1f7c5149b735e0d277494c6e4c1435d
HiLaVine <sinnafa01@gmail.com>
2024-11-09 18:34:37 -0600

    2.0.8

--- modulo2/variables.js
+++ modulo2/variables.js
@@ -29,7 +29,16 @@ console.log(steps);  //  ->  320*/
 let counter = 100;
 
 console.log(greeting + counter);*/
-
+/*
 const greeting = "hello";
-greeting = "hi";
+greeting = "hi";*/
+
+let height = 180;
+{
+    let weight = 70;
+    console.log(height); // -> 180
+    console.log(weight); // -> 70
+}
+console.log(height); // -> 180
+console.log(weight); // -> Uncaught ReferenceError: weight is not defined
 
