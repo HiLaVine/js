fc72970a61c091bab910ef903e12df0504d5e395
HiLaVine <sinnafa01@gmail.com>
2024-11-10 23:29:35 -0600

    Modulo 2.0 terminado

--- modulo2/variables.js
+++ modulo2/variables.js
@@ -33,12 +33,87 @@ console.log(greeting + counter);*/
 const greeting = "hello";
 greeting = "hi";*/
 
-let height = 180;
+/*var  height  =  180;
 {
-    let weight = 70;
-    console.log(height); // -> 180
-    console.log(weight); // -> 70
+         var  weight  =  70;
+         console.log(height);  //  ->  180
+         console.log(weight);  //  ->  70	
 }
-console.log(height); // -> 180
-console.log(weight); // -> Uncaught ReferenceError: weight is not defined
+console.log(height);  //  ->  180
+console.log(weight);  //  ->  70*/
 
+/*function suma (a , b){
+    return a+b;
+}
+
+console.log(suma(10 , 20));*/
+
+/*var  globalGreeting  =  "Good  ";
+   
+function  testFunction()  {
+         var  localGreeting  =  "Morning  ";    
+         console.log("function:");
+         console.log(globalGreeting);
+         console.log(localGreeting);
+}
+   
+testFunction();
+   
+console.log("main  program:");
+console.log(globalGreeting);
+console.log(localGreeting);  //  ->  Uncaught  ReferenceError:  localGreeting  is  not  defined*/
+
+/*-----------------------Example of the exercise--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*const ROSE_PRICE = 8;
+const LILY_PRICE = 10;
+const TULIP_PRICE = 2;
+
+let quantityOfRoses = 70;
+let quantityOfLilies = 50;
+let quantityOfTulips = 120;
+
+let totalValueOfRoses = ROSE_PRICE * quantityOfRoses;
+let totalValueOfLilies = LILY_PRICE * quantityOfLilies;
+let totalValueOfTulips = TULIP_PRICE * quantityOfTulips;
+
+let total = totalValueOfRoses + totalValueOfLilies + totalValueOfTulips;
+
+console.log("Rose - unit price: " + ROSE_PRICE + " , " + "quantity: " + quantityOfRoses + " , " + "value: " + totalValueOfRoses);
+console.log("Lily - unit price: " + LILY_PRICE + " , " + "quantity: " + quantityOfLilies + " , " + "value: " + totalValueOfLilies);
+console.log("Tulip - unit price: " + TULIP_PRICE + " , " + "quantity: " + quantityOfTulips + " , " + "value: " + totalValueOfTulips);
+console.log("Total: "+ total);
+
+quantityOfRoses = quantityOfRoses-20;
+quantityOfLilies = quantityOfLilies-30;
+
+totalValueOfRoses = ROSE_PRICE * quantityOfRoses;
+totalValueOfLilies = LILY_PRICE * quantityOfLilies;
+totalValueOfTulips = TULIP_PRICE * quantityOfTulips;
+
+total = totalValueOfRoses + totalValueOfLilies + totalValueOfTulips;
+
+console.log("Rose - unit price: " + ROSE_PRICE + " , " + "quantity: " + quantityOfRoses + " , " + "value: " + totalValueOfRoses);
+console.log("Lily - unit price: " + LILY_PRICE + " , " + "quantity: " + quantityOfLilies + " , " + "value: " + totalValueOfLilies);
+console.log("Tulip - unit price: " + TULIP_PRICE + " , " + "quantity: " + quantityOfTulips + " , " + "value: " + totalValueOfTulips);
+console.log("Total: "+ total);*/
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------Exercise------------------------------------------------------------------------------------------------------------------------------------------------*/
+const NAME_ONE = "Maxwell Wrigth";
+const NAME_TWO = "Raja Villareal";
+const NAME_THREE = "Helen Richard";
+
+let phoneOne = "(0191) 719 6495";
+let phoneTwo = "0866 698 2895";
+let phoneThree = "0800 1111";
+
+let emailOne = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let emailTwo = "posuere.vulputate@sed.com";
+let emailThree = "libero@convallis.edu";
+
+console.log("Name: " + NAME_ONE + " , " + "Phone: " + phoneOne + " , " + "Email: " + emailOne);
+console.log("Name: " + NAME_TWO + " , " + "Phone: " + phoneTwo + " , " + "Email: " + emailTwo);
+console.log("Name: " + NAME_THREE + " , " + "Phone: " + phoneThree + " , " + "Email: " + emailThree);
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
